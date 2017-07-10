c8a8a88b614b0612555624f9a765f112880866b4
Charles Burch <charlesburch@CBMacBook-Pro.local>
2017-07-10 16:12:29 -0500

    association test created

---
+++ users/test/association_test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+const User = require('../src/user');
+const Comment = require('../src/comment');
+const BlogPost = require('../src/blogPost');
+
+
+describe('Assocations')
