c9cbe1c98b1d54f55210625aae1ae2e5fbc10939
Charles Burch <charlesburch@CBMacBook-Pro.local>
2017-07-10 05:32:28 -0500

    test add to make sure invalid records are not save to the database

--- users/test/validation_test.js
+++ users/test/validation_test.js
@@ -23,5 +23,14 @@ it('requires a user name',(done)=>{
         assert(message === 'Name must be longer than 2 characters.');
         done();
     });
+    it('disallows invalid records from being saved',(done)=>{
+        const user = new User({name: 'Al'});
+        user.save()
+            .catch((validationResult)=>{
+            const {message} = validationResult.errors.name;
 
+            assert(message === 'Name must be longer than 2 characters.');
+            done();
+        }); 
+    });
 });
