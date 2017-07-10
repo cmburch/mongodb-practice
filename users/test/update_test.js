3faace0feebe839bb6731300c71db865b3063151
Charles Burch <charlesburch@CBMacBook-Pro.local>
2017-07-10 14:18:32 -0500

    UserSchema virtual postcreated and likes add to schema

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -6,7 +6,7 @@ describe('Updating records',()=>{
 let joe;
 
     beforeEach((done)=>{
-        joe = new User({name:'Joe',postCount:0});
+        joe = new User({name:'Joe',likes:0});
         joe.save()
         .then(()=>done());
 
@@ -39,11 +39,11 @@ function assertName(operation,done){
     it('A model class can update a record with Id and update',(done)=>{
       assertName(User.findByIdAndUpdate(joe._id,{name:'Alex'}),done);
     });
-    xit('A user can have their post count incremented by 1',(done)=>{
-        User.update({name: 'Joe'},{$inc: { postCount: 1}})
+    it('A user can have their post count incremented by 1',(done)=>{
+        User.update({name: 'Joe'},{$inc: {likes: 1}})
             .then(()=> User.findOne({name:'Joe'}))
             .then((user) =>{
-                assert(user.postCount === 1);
+                assert(user.likes === 1);
                 done();
             });
     });
