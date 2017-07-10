445d4dcf5aec99beba5cce337814e771052c788c
Charles Burch <charlesburch@CBMacBook-Pro.local>
2017-07-10 02:56:21 -0500

    test added to check to see post count increments when updated

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -6,7 +6,7 @@ describe('Updating records',()=>{
 let joe;
 
     beforeEach((done)=>{
-        joe = new User({name:'Joe'});
+        joe = new User({name:'Joe',postCount:0});
         joe.save()
         .then(()=>done());
 
@@ -23,8 +23,6 @@ function assertName(operation,done){
     });
 }
 
-
-
     it('instance type using set and save',(done)=>{
         joe.set('name','Alex');
         assertName(joe.save(),done);
@@ -40,5 +38,12 @@ function assertName(operation,done){
     });
     it('A model class can update a record with Id and update',(done)=>{
       assertName(User.findByIdAndUpdate(joe._id,{name:'Alex'}),done);
+    });
+    it('A user can have their post count incremented by 1',()=>{
+        User.update({name: 'Joe'},{$inc: { postCount: 1}})
+            .then(()=> User.findOne({name:'Joe'}))
+            .then((user) =>{
+                assert(user.postCount ===1);
+            });
     });
 });
