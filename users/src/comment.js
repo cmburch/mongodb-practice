2ef239cec644a17527ee44ce66efbae001547459
Charles Burch <charlesburch@CBMacBook-Pro.local>
2017-07-10 15:58:56 -0500

    comment model created

--- users/src/comment.js
+++ users/src/comment.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const BlogPostSchema = new Schema({
-    title:String,
+const CommentSchema = new Schema({
     content:String,
-    comments:[{
+    user:[{
         type:Schema.Types.ObjectId,
-        ref: 'comment'
+        ref: 'user'
     }]
 });
 
